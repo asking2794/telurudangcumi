1e4dd9022dc537b499b3483c5c8f1f6efe45e698
asking2794 <146243355+asking2794@users.noreply.github.com>
2024-12-20 22:02:31 +0700

    Create tracking.js

---
+++ tracking.js
@@ -0,0 +1,16 @@
+//Histats
+document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4917504&101"/>');
+
+///// Statcounter /////
+var sc_project=13071171; 
+var sc_invisible=1; 
+var sc_security="ebde8f10";
+
+document.write('<script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>');
+document.write('<noscript>');
+document.write('<div class="statcounter">');
+document.write('<a title="Web Analytics" href="https://statcounter.com/" target="_blank">');
+document.write('<img class="statcounter" src="https://c.statcounter.com/13071171/0/ebde8f10/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade">');
+document.write('</a>');
+document.write('</div>');
+document.write('</noscript>');
