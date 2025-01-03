a235160c17186750181504ab6db3ef7ff1f42faa
asking2794 <146243355+asking2794@users.noreply.github.com>
2025-01-04 01:13:29 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,9 +1,9 @@
 
  atOptions = {
-		'key' : 'c0e7ae62487c201244859c735cc89d8a',
+		'key' : '7b8bc179ccc7816266dd1427c3863bb0',
 		'format' : 'iframe',
 		'height' : 90,
 		'width' : 728,
 		'params' : {}
 	};
-document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/c0e7ae62487c201244859c735cc89d8a/invoke.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/7b8bc179ccc7816266dd1427c3863bb0/invoke.js"></scr' + 'ipt>');
