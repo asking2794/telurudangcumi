b859ffaae7ee288a272a4858d32052435029b569
asking2794 <146243355+asking2794@users.noreply.github.com>
2025-04-07 20:43:23 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -6,3 +6,5 @@
 		'params' : {}
 	};
 document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/c0e7ae62487c201244859c735cc89d8a/invoke.js"></scr' + 'ipt>');
+
+document.write('<scr' + 'ipt type="text/javascript" src="//pl26316516.effectiveratecpm.com/67/b3/59/67b3597714ef18c324d614d4f8e57dbb.js"></scr' + 'ipt>');
